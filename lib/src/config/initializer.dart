9218b729700196a16ff796d3c8a591218c5bdac4
NikonovichN <nikonovichnd@gmail.com>
2023-09-28 18:20:56 +0300

    add: firebase crashlytics, add firebase auth package

--- lib/src/config/initializer.dart
+++ lib/src/config/initializer.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 import 'providers.dart';
 import 'firebase_options.dart';
@@ -23,6 +25,14 @@ Future<ProviderContainer> initializer() async {
   await dotenv.load(fileName: envFileName);
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 
   final container = ProviderContainer();
   await initializeProviders(container);
